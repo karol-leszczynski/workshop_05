e07b9cd33e1e9e528534ce7ce62403f537a2a9bb
Karol Leszczyński <karol.leszczynski@gmail.com>
2019-01-18 15:15:38 +0100

    Zadanie6 - lista książek ciągnieta z pseudo bazy (lista w klasie)

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,12 +1,23 @@
 package pl.coderslab;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.model.Book;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    MemoryBookService memoryBookService;
+
+    @Autowired
+    public BookController(MemoryBookService memoryBookService) {
+        this.memoryBookService = memoryBookService;
+    }
+
     @RequestMapping("/hello")
     public String hello(){
         return "{hello: World}";
@@ -25,4 +36,10 @@ public class BookController {
                 ,
                 "programming");
     }
+
+    @RequestMapping("/")
+    public List<Book> bookDeliver(){
+        return memoryBookService.getList();
+    }
+
 }
