17fdf256aa242a13ad5c655dcef958c8bf4db1f4
Karol Leszczyński <karol.leszczynski@gmail.com>
2019-01-18 14:43:47 +0100

    Zadanie5 - lista książek z inicjalizacją

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -2,6 +2,7 @@ package pl.coderslab;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.model.Book;
 
 @RestController
 @RequestMapping("/books")
@@ -9,5 +10,19 @@ public class BookController {
     @RequestMapping("/hello")
     public String hello(){
         return "{hello: World}";
+    }
+
+    @RequestMapping("/helloBook")
+    public Book helloBook(){
+        return new Book(1L,
+                "9788324631766"
+                ,
+                "Thinking in Java"
+                ,
+                "Bruce Eckel"
+                ,
+                "Helion"
+                ,
+                "programming");
     }
 }
